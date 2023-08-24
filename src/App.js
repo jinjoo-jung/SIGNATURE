14577d746a4aed6aa214e0c29d9bd5762c1a9d45
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-24 13:15:33 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-export default class App {
-  constructor() {
-    this.el = document.createElement("div");
+// export default class App {
+//   constructor() {
+//     this.el = document.createElement("div");
+//     this.el.textContent = "Hello, world!";
+//   }
+// }
+
+import { Component } from "./core/heropy";
+
+export default class App extends Component {
+  render() {
     this.el.textContent = "Hello, world!";
   }
 }
